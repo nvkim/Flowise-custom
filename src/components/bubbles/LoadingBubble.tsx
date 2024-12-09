f53479f3e31dcbf0d20377d97de1e149515fa390
Kim <khiem.ngo.v@gmail.com>
2024-12-09 20:59:33 +0700

    Update version to 1.0.12, modify chatbot configuration, and refine LoadingBubble component
    - Bumped version in package.json to 1.0.12.
    - Updated chatbot configuration in index.html with a new chatflow ID and API host.
    - Refined the LoadingBubble component by removing unnecessary CSS classes for improved clarity.

--- src/components/bubbles/LoadingBubble.tsx
+++ src/components/bubbles/LoadingBubble.tsx
@@ -2,7 +2,7 @@ import { TypingBubble } from '@/components';
 
 export const LoadingBubble = () => (
   <div class="flex justify-start mb-2 items-start animate-fade-in host-container">
-    <span class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble" data-testid="host-bubble">
+    <span class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full" data-testid="host-bubble">
       <TypingBubble />
     </span>
   </div>
